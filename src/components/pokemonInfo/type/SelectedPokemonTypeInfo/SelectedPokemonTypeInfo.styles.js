0feb04235819eef1d3a1fe08e0b8450d690e29f7
Michael Melis <michael.melis169@gmail.com>
2022-01-14 19:26:55 +0100

    style: edit type styling

--- src/components/pokemonInfo/type/SelectedPokemonTypeInfo/SelectedPokemonTypeInfo.styles.js
+++ src/components/pokemonInfo/type/SelectedPokemonTypeInfo/SelectedPokemonTypeInfo.styles.js
@@ -2,7 +2,16 @@ import styled from "styled-components";
 
 export const StyledSelectedPokemonTypeInfo = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   align-items: center;
   flex-wrap: wrap;
+  /* border: 1px solid black; */
+  border-radius: 15px;
+  box-shadow: rgba(17, 17, 26, 0.1) 0px 1px 0px,
+    rgba(17, 17, 26, 0.1) 0px 8px 24px, rgba(17, 17, 26, 0.1) 0px 16px 48px;
+  padding: 1rem;
+  margin: 1rem;
+  h2 {
+    width: 20vw;
+  }
 `;
