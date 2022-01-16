7e0baa53df9baf4ca8d06cd8e8b4876f596b5bef
Michael Melis <michael.melis169@gmail.com>
2022-01-16 18:27:09 +0100

    build: basic stats added

--- src/components/pokemonInfo/basicInfo/PokemonBasicInfo.jsx
+++ src/components/pokemonInfo/basicInfo/PokemonBasicInfo.jsx
@@ -1,21 +1,43 @@
 import React from "react";
-import { StyledBasicInfo } from "./PokemonBasicInfo.styles";
+import { StyledBasicInfo, StyledStats } from "./PokemonBasicInfo.styles";
+import { nanoid } from "nanoid";
 
 const PokemonBasicInfo = ({ pokemonImg, selectedPokemonInfo }) => {
+  console.log(selectedPokemonInfo);
   return (
-    <StyledBasicInfo>
-      <h1>
-        You choosed:
-        <span>
-          #{selectedPokemonInfo.id}{" "}
-          {`${selectedPokemonInfo.name
-            .charAt(0)
-            .toUpperCase()}${selectedPokemonInfo.name.slice(1)}`}
-        </span>
-      </h1>
+    <div>
+      <StyledBasicInfo>
+        <h1>
+          You choosed:
+          <span>
+            #{selectedPokemonInfo.id}{" "}
+            {`${selectedPokemonInfo.name
+              .charAt(0)
+              .toUpperCase()}${selectedPokemonInfo.name.slice(1)}`}
+          </span>
+        </h1>
 
-      <img src={pokemonImg} alt={selectedPokemonInfo.name} />
-    </StyledBasicInfo>
+        <img src={pokemonImg} alt={selectedPokemonInfo.name} />
+      </StyledBasicInfo>
+      <div>
+        {selectedPokemonInfo.stats.map((obj) => (
+          <StyledStats key={nanoid()}>
+            <div>
+              <h2>{obj.stat.name}</h2>
+            </div>
+            <div>
+              <p
+                style={{
+                  background: `-webkit-linear-gradient(left, #70d9dd ${obj["base_stat"]}%, white 0%)`,
+                }}
+              >
+                {obj["base_stat"]}
+              </p>
+            </div>
+          </StyledStats>
+        ))}
+      </div>
+    </div>
   );
 };
 
--- src/components/pokemonInfo/basicInfo/PokemonBasicInfo.styles.jsx
+++ src/components/pokemonInfo/basicInfo/PokemonBasicInfo.styles.jsx
@@ -3,10 +3,21 @@ import styled from "styled-components";
 export const StyledBasicInfo = styled.div`
   display: flex;
   flex-wrap: wrap;
-  flex-direction: row;
+  flex-direction: column;
   align-items: center;
   justify-content: space-between;
   span {
     font-weight: bolder;
+  }
+`;
+
+export const StyledStats = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  p {
+    width: 50vw;
+    border: 2px solid black;
   }
 `;
